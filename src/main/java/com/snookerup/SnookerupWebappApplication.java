11bfe83a30d79ad6821541ca5efe182c27802b84
Huw Carpenter <huw@Huws-MacBook-Air.local>
2024-12-20 19:32:41 +0000

    Initial commit

---
+++ src/main/java/com/snookerup/SnookerupWebappApplication.java
@@ -0,0 +1,13 @@
+package com.snookerup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnookerupWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnookerupWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snookerup/SnookerupWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snookerup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnookerupWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
